7a5faec460e38efe24ae1fefbfaa25ed1a7076bb
Alex <alex@localhost.localdomain>
2024-08-20 10:37:06 +0200

    ajout script

---
+++ dictionnaire/target/debug/deps/dictionnaire-aa59ca088fdb58be.d
@@ -0,0 +1,5 @@
+/home/alex/Developpement/Rust/Tutos/dictionnaire/target/debug/deps/libdictionnaire-aa59ca088fdb58be.rmeta: src/main.rs
+
+/home/alex/Developpement/Rust/Tutos/dictionnaire/target/debug/deps/dictionnaire-aa59ca088fdb58be.d: src/main.rs
+
+src/main.rs:
